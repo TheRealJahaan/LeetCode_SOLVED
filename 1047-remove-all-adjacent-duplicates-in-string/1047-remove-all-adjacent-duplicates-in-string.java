953633898f65e5550bd42c11ca80fe7f8dd647cf
Jahaan Suthar <180190128+TheRealJahaan@users.noreply.github.com>
2026-07-18 16:43:41 +0530

    Time: 14 ms (77.47%), Space: 46 MB (99.43%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,17 +1,20 @@
 class Solution {
     public String removeDuplicates(String s) {
+        // using StringBuilder for solving it 
         StringBuilder ans = new StringBuilder() ;
-
+//      ab hum 0 --> s.length tak jayenge aur character nikalaenge
         for(int i = 0 ; i <s.length() ; i++){
             char ch = s.charAt(i) ;
-
+            // agar ans ka last digit equal hota he ch ke to delete marenge 
             if( ans.length() > 0 && ans.charAt(ans.length() - 1) == ch){
                 ans.deleteCharAt(ans.length() - 1) ;
             }
+            // varna ans me vo ch daal do 
             else{
                 ans.append(ch) ;
             }
         }
+        // aur fir StringBuilder ko String me convert kar denge
         return ans.toString(); 
     }
 }
