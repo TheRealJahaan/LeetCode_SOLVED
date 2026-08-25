0030039f504301fba0d8ef488b5846f5ae8837d4
Jahaan Suthar <180190128+TheRealJahaan@users.noreply.github.com>
2026-08-25 13:34:43 +0530

    Time: 18 ms (76.79%), Space: 108.2 MB (46.35%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,7 +1,7 @@
 class Solution {
-    public boolean containsDuplicate(int[] arr) {
-        int n = arr.length ;
-        Arrays.sort(arr) ;
+    // public boolean containsDuplicate(int[] arr) {
+    //     int n = arr.length ;
+    //     Arrays.sort(arr) ;
 
         // for(int i = 0 ; i < n ; i++){          |
         //     for(int j = i + 1 ; j < n ; j++){  |
@@ -17,14 +17,30 @@ class Solution {
         // 1.) Arrays to sort to kar hi diya h to ek test case lete he
         // {1,1,1,2,2,3,3,3,4,4,4,4} ;
 
-        for(int i = 0 ; i < n - 1; i++){
-            if(arr[i] == arr[i + 1]){
-            return true ;
+    //     for(int i = 0 ; i < n - 1; i++){
+    //         if(arr[i] == arr[i + 1]){
+    //         return true ;
                 
-            }
+    //         }
 
+    //     }
+    //     return false ;
+
+    // }
+
+    // Using Set method
+
+    public boolean containsDuplicate(int arr[]) {
+        Set<Integer> set = new HashSet<>() ;
+
+        for(int i : arr){
+            if(set.contains(i)){
+                return true  ;
+            }
+            set.add(i) ;
         }
         return false ;
-
     }
+    
+
 }
