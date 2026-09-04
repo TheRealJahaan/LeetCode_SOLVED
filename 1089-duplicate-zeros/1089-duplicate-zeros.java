5de3363b03ede4dbdb375b6db08a524944aab259
Jahaan Suthar <180190128+TheRealJahaan@users.noreply.github.com>
2026-09-04 16:07:36 +0530

    Time: 1 ms (99.44%), Space: 47.1 MB (23.26%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,31 +1,31 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        // int lastidx = arr.length - 1 ;
-        // int possibleZeroDups = 0 ;
+        int lastidx = arr.length - 1 ;
+        int possibleZeroDups = 0 ;
 
-        // for(int i = 0 ; i <= lastidx - possibleZeroDups ; i++){
-        //     if(arr[i] == 0 ){
-        //         // Edge Case 
-        //         if(i == lastidx - possibleZeroDups){
-        //             arr[lastidx] = 0 ;
-        //             lastidx -= 1 ;
-        //             break;
-        //         }
-        //         possibleZeroDups++ ;
-        //     }
-        // }
-        // int newlastidx = lastidx - possibleZeroDups ;
+        for(int i = 0 ; i <= lastidx - possibleZeroDups ; i++){
+            if(arr[i] == 0 ){
+                // Edge Case 
+                if(i == lastidx - possibleZeroDups){
+                    arr[lastidx] = 0 ;
+                    lastidx -= 1 ;
+                    break;
+                }
+                possibleZeroDups++ ;
+            }
+        }
+        int newlastidx = lastidx - possibleZeroDups ;
 
-        // for(int i = newlastidx ; i>= 0 ; i--){
-        //     if(arr[i] == 0) {
-        //         arr[i + possibleZeroDups] = 0 ;
-        //         possibleZeroDups-- ;
-        //         arr[i + possibleZeroDups] = 0 ;
-        //     }
-        //     else{
-        //         arr[i + possibleZeroDups] = arr[i] ;
-        //     }
-        // }
+        for(int i = newlastidx ; i>= 0 ; i--){
+            if(arr[i] == 0) {
+                arr[i + possibleZeroDups] = 0 ;
+                possibleZeroDups-- ;
+                arr[i + possibleZeroDups] = 0 ;
+            }
+            else{
+                arr[i + possibleZeroDups] = arr[i] ;
+            }
+        }
 
 
             // ITERATIVE METHOD 
@@ -58,33 +58,5 @@ class Solution {
         // for(int i = 0 ; i< arr.length ; i++){
         //     arr[i] = dest[i] ;
         // }
-
-        // The correct method to approsch this solutuion 
-
-        int lastidx = arr.length - 1 ;
-        int zerodup = 0 ;
-
-        for(int i = 0 ; i <= lastidx - zerodup ; i++){
-            if(arr[i] == 0 ){
-                if(i == lastidx - zerodup){
-                    arr[lastidx] = 0 ;
-                    lastidx = lastidx - 1 ;
-                    break;
-                }
-                zerodup++ ;
-            }
-        }
-        // ab yaha tak mere paas ye scene he ki agar last index me zero hua to bhi last index me 0 sotre ho jayega and then if not then zero jitne he utne ko lastindex se minus to ho hi raha he 
-        int newLastindex = lastidx - zerodup ;
-        for(int i = newLastindex ; i >= 0 ; i--){
-            if(arr[i] == 0) {
-                arr[i + zerodup] = 0 ;
-                zerodup-- ;
-                arr[i + zerodup] = 0 ;
-            }
-            else{
-                arr[i + zerodup] = arr[i] ;
-            }
-        }
     }
 }
