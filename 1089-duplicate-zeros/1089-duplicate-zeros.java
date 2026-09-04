66d103f8d1616aa737e8f71c259ec57be3ad6a72
Jahaan Suthar <180190128+TheRealJahaan@users.noreply.github.com>
2026-09-04 16:07:15 +0530

    Time: 1 ms (99.44%), Space: 47.2 MB (8.9%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -30,33 +30,61 @@ class Solution {
 
             // ITERATIVE METHOD 
 
-        int [] dest = new int [arr.length];
+        // int [] dest = new int [arr.length];
 
-        int s = 0 ;
-        int d = 0 ;
+        // int s = 0 ;
+        // int d = 0 ;
 
-        while ( s< arr.length ){
-            if(arr[s] == 0 ){
-                if(d < arr.length ){
-                    dest[d] = 0 ;   
+        // while ( s< arr.length ){
+        //     if(arr[s] == 0 ){
+        //         if(d < arr.length ){
+        //             dest[d] = 0 ;   
+        //         }
+        //         d = d +1 ;
+        //         if(d < arr.length ){
+        //             dest[d] = 0;
+        //         }
+
+        //     }
+        //     else{
+        //         if(d < arr.length){
+        //             dest[d] = arr[s] ;
+        //         }
+        //     }
+        //     d = d+ 1 ;
+        //     s = s+ 1 ;
+        // }
+
+        // for(int i = 0 ; i< arr.length ; i++){
+        //     arr[i] = dest[i] ;
+        // }
+
+        // The correct method to approsch this solutuion 
+
+        int lastidx = arr.length - 1 ;
+        int zerodup = 0 ;
+
+        for(int i = 0 ; i <= lastidx - zerodup ; i++){
+            if(arr[i] == 0 ){
+                if(i == lastidx - zerodup){
+                    arr[lastidx] = 0 ;
+                    lastidx = lastidx - 1 ;
+                    break;
                 }
-                d = d +1 ;
-                if(d < arr.length ){
-                    dest[d] = 0;
-                }
-
+                zerodup++ ;
+            }
+        }
+        // ab yaha tak mere paas ye scene he ki agar last index me zero hua to bhi last index me 0 sotre ho jayega and then if not then zero jitne he utne ko lastindex se minus to ho hi raha he 
+        int newLastindex = lastidx - zerodup ;
+        for(int i = newLastindex ; i >= 0 ; i--){
+            if(arr[i] == 0) {
+                arr[i + zerodup] = 0 ;
+                zerodup-- ;
+                arr[i + zerodup] = 0 ;
             }
             else{
-                if(d < arr.length){
-                    dest[d] = arr[s] ;
-                }
+                arr[i + zerodup] = arr[i] ;
             }
-            d = d+ 1 ;
-            s = s+ 1 ;
-        }
-
-        for(int i = 0 ; i< arr.length ; i++){
-            arr[i] = dest[i] ;
         }
     }
 }
