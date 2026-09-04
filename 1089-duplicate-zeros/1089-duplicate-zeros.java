74957cf3ff3c5a411a936347302694084bece8dc
Jahaan Suthar <180190128+TheRealJahaan@users.noreply.github.com>
2026-09-04 15:49:38 +0530

    Time: 1 ms (99.44%), Space: 47 MB (41.99%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,31 +1,62 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int lastidx = arr.length - 1 ;
-        int possibleZeroDups = 0 ;
+        // int lastidx = arr.length - 1 ;
+        // int possibleZeroDups = 0 ;
 
-        for(int i = 0 ; i <= lastidx - possibleZeroDups ; i++){
-            if(arr[i] == 0 ){
-                // Edge Case 
-                if(i == lastidx - possibleZeroDups){
-                    arr[lastidx] = 0 ;
-                    lastidx -= 1 ;
-                    break;
+        // for(int i = 0 ; i <= lastidx - possibleZeroDups ; i++){
+        //     if(arr[i] == 0 ){
+        //         // Edge Case 
+        //         if(i == lastidx - possibleZeroDups){
+        //             arr[lastidx] = 0 ;
+        //             lastidx -= 1 ;
+        //             break;
+        //         }
+        //         possibleZeroDups++ ;
+        //     }
+        // }
+        // int newlastidx = lastidx - possibleZeroDups ;
+
+        // for(int i = newlastidx ; i>= 0 ; i--){
+        //     if(arr[i] == 0) {
+        //         arr[i + possibleZeroDups] = 0 ;
+        //         possibleZeroDups-- ;
+        //         arr[i + possibleZeroDups] = 0 ;
+        //     }
+        //     else{
+        //         arr[i + possibleZeroDups] = arr[i] ;
+        //     }
+        // }
+
+
+            // ITERATIVE METHOD 
+
+        int [] dest = new int [arr.length];
+
+        int s = 0 ;
+        int d = 0 ;
+
+        while ( s< arr.length ){
+            if(arr[s] == 0 ){
+                if(d < arr.length ){
+                    dest[d] = 0 ;   
                 }
-                possibleZeroDups++ ;
-            }
-        }
-        int newlastidx = lastidx - possibleZeroDups ;
+                d = d +1 ;
+                if(d < arr.length ){
+                    dest[d] = 0;
+                }
 
-        for(int i = newlastidx ; i>= 0 ; i--){
-            if(arr[i] == 0) {
-                arr[i + possibleZeroDups] = 0 ;
-                possibleZeroDups-- ;
-                arr[i + possibleZeroDups] = 0 ;
             }
             else{
-                arr[i + possibleZeroDups] = arr[i] ;
+                if(d < arr.length){
+                    dest[d] = arr[s] ;
+                }
             }
+            d = d+ 1 ;
+            s = s+ 1 ;
         }
 
+        for(int i = 0 ; i< arr.length ; i++){
+            arr[i] = dest[i] ;
+        }
     }
 }
