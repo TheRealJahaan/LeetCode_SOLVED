819276e114f0332395fa558022d5389081c583ad
Jahaan Suthar <180190128+TheRealJahaan@users.noreply.github.com>
2026-09-05 11:26:41 +0530

    Time: 0 ms (100%), Space: 46.4 MB (96.5%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -12,21 +12,41 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
 
-        // ===========APPROACH 1======================
+        // // ===========APPROACH 1======================
 
-        HashMap<ListNode , Boolean> map = new HashMap<>() ;
+        // HashMap<ListNode , Boolean> map = new HashMap<>() ;
 
-        ListNode current = head ;
+        // ListNode current = head ;
 
-        while(current != null){
-            if(map.containsKey(current)){
+        // while(current != null){
+        //     if(map.containsKey(current)){
+        //         return true ;
+        //     }
+        //     map.put(current , true) ;
+        //     current = current.next ;
+        // }
+        // return false; 
+
+        // ========OPTIMIZED APPROACH 2 ================
+
+        // Using the slow and fast (tortoise hare appoarch )
+        // Baat yaha esi he ki agar different speed he and cycle my bhaag rahe he then it is 100% gurantee that sooner or later they will meet 
+        // That's the complete point here
+
+        ListNode fast = head ;
+        ListNode slow = head ;
+
+        while(fast != null){
+            fast = fast.next ;
+            if(fast != null){
+                fast = fast.next ;
+                slow = slow.next ;
+            }
+            if(fast == slow){
                 return true ;
             }
-            map.put(current , true) ;
-            current = current.next ;
         }
-        return false; 
-
+        return false ;
 
     }
 }
