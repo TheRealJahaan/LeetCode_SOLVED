f1f98c9f18daebe8762f9f3f81897ec2c838361f
Jahaan Suthar <180190128+TheRealJahaan@users.noreply.github.com>
2026-09-05 13:27:46 +0530

    Time: 0 ms (100%), Space: 43.5 MB (58.62%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseLL(ListNode head) {
+        ListNode prevNode = null ;
+        ListNode currentNode = head ;
+        while(currentNode != null){
+            ListNode forwardNode = currentNode.next ;
+
+            currentNode.next = prevNode;
+            prevNode = currentNode ;
+            currentNode = forwardNode ;
+        }
+        return prevNode ;
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // Removing nthe from the end is basically removing moving size - n time to the list 
+
+
+        head = reverseLL(head) ;
+//         single Node case solved
+        if(n == 1 ) {
+            head = head.next ;
+            return reverseLL(head) ;
+        }
+        // Now main Logic for n > 1 nodes 
+        // hamne reverse to kar diya 
+        ListNode prev = head ;
+        for(int i = 1 ; i < n - 1  ; i++){
+            prev = prev.next ;
+        }
+        // ListNode current = prev.next ;
+        // ListNode forward = prev.next.next ;
+        prev.next = prev.next.next ;
+
+        return reverseLL(head); 
+
+    }
+}
