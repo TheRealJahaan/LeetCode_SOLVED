1ef54ef2c80d4228e0f0b4e5908ffae83f14d511
Jahaan Suthar <180190128+TheRealJahaan@users.noreply.github.com>
2026-09-10 11:59:41 +0530

    Time: 0 ms (100%), Space: 44.6 MB (10.07%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -9,60 +9,94 @@
  * }
  */
 class Solution {
-    public ListNode reverseLL(ListNode head ) {
-        ListNode prevNode= null ;
-        ListNode currentNode = head ;
+    // public ListNode reverseLL(ListNode head ) {
+    // //     ListNode prevNode= null ;
+    // //     ListNode currentNode = head ;
 
-        while(currentNode != null){
-            ListNode forwardNode = currentNode.next;
-            currentNode.next = prevNode ;
-            prevNode = currentNode ;
-            currentNode = forwardNode ;
-        }
-        return prevNode ;
-    }
+    // //     while(currentNode != null){
+    // //         ListNode forwardNode = currentNode.next;
+    // //         currentNode.next = prevNode ;
+    // //         prevNode = currentNode ;
+    // //         currentNode = forwardNode ;
+    // //     }
+    // //     return prevNode ;
+    // // }
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next == null || k == 0 ){
+    //     if(head == null || head.next == null || k == 0 ){
+    //         return head ;
+    //     }
+
+    //     // size nikalni padegi 
+    //     ListNode temp= head ;
+    //     int n = 0 ;
+    //     while(temp != null ){
+    //         n++ ;
+    //         temp = temp.next;
+    //     }
+    //     k = k % n ;
+    //     if(k == 0 ){
+    //         return head ;
+    //     }
+    //     // reversing
+    //     head = reverseLL(head) ;
+    //     // reverse to ho gaya he 
+    //      // ab ye logic karna he
+    //     ListNode first = head ;
+    //     ListNode second = head ;
+
+    //     for(int i = 1 ; i < k ; i++){
+    //         second = second.next ;
+    //     }
+    //     ListNode remaining = second.next ;
+    //     second.next = null ;
+
+    //     first = reverseLL(first) ;
+    //     remaining = reverseLL(remaining) ;
+
+    //     ListNode tail = first ;
+
+    //     while(tail.next != null){
+    //         tail = tail.next ;
+    //     } 
+    //     tail.next = remaining ;
+
+    //     return first ;
+
+    //   ================== OPTIMIZED APPROACH=========================
+
+        // Make the List circular first 
+
+        //   |->(10) -> (20) -> (30) -> (40) --|
+        //   |                                 |
+    //       |_________________________________|
+
+         
+        //  if k = 1 to (30) vali node ko break karta he which is n-k vali ki next ko null par point karo 
+        if(head == null || k == 0 ){
             return head ;
         }
 
-        // size nikalni padegi 
         ListNode temp= head ;
-        int n = 0 ;
-        while(temp != null ){
+        int n = 1 ;
+        while(temp.next != null){
             n++ ;
-            temp = temp.next;
+            temp = temp.next ;
         }
+        
+        temp.next = head ;
+        // circular ban gaya he 
+        
+        // size pata honi chahiye 
         k = k % n ;
-        if(k == 0 ){
-            return head ;
+        temp = head ;
+        for(int i = 1 ; i <= n - k -1 ; i++){
+            temp = temp.next ;
+            head = head.next;
         }
-        // reversing
-        head = reverseLL(head) ;
-        // reverse to ho gaya he 
-         // ab ye logic karna he
-        ListNode first = head ;
-        ListNode second = head ;
+        head = head.next ;
+        temp.next = null ;
 
-        for(int i = 1 ; i < k ; i++){
-            second = second.next ;
-        }
-        ListNode remaining = second.next ;
-        second.next = null ;
-
-        first = reverseLL(first) ;
-        remaining = reverseLL(remaining) ;
-
-        ListNode tail = first ;
-
-        while(tail.next != null){
-            tail = tail.next ;
-        } 
-        tail.next = remaining ;
-
-        return first ;
-
-      
+        return head  ;
     }
 
 }
